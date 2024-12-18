d0978e0869adefee2ba6f05b277608f6ca926f70
Ruvi Cohen <r0583247229@gmail.com>
2024-12-18 11:38:39 +0200

    add sql settings

---
+++ app/db/sql_database.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy_utils import database_exists, create_database
+
+load_dotenv(verbose=True)
+
+database_url = os.environ['POSTGRES_URL']
+engine = create_engine(database_url)
+session_maker = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+def create_db():
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+def create_tables():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
